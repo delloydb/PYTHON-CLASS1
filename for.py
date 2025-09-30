cbeddf4325a63b9e9ccf915c02a9a8f852607a28
Lloyd <lloydbrownjnr@gmail.com>
2025-09-30 11:28:00 +0300

    for loop concepts

--- for.py
+++ for.py
@@ -1,2 +1,14 @@
 for number in range(6):
     print("Current number:", number)
+
+# for else
+for num in range(6):
+    print("Current number:", num)
+else:
+    print("Loop is over")
+
+
+# nested for loop
+for i in range(3):
+    for j in range(2):
+        print(f"{i}, {j}")
