3224daaf837c5dbf47d80f66420758c1e06b62a8
Lloyd <lloydbrownjnr@gmail.com>
2025-09-30 11:00:22 +0300

    if condtional statements in python

---
+++ if.py
@@ -0,0 +1,11 @@
+temp = 9
+if temp > 20:
+    print("It's a nice day")
+    print("Drink plenty of water")
+elif temp < 10:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's neither hot nor cold")
+    print("Have a nice day")
+print("Enjoy your day")
