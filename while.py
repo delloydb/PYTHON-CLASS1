6c0ad9cec10b069cb419989883843c7b94b9b20e
Lloyd <lloydbrownjnr@gmail.com>
2025-09-30 11:31:20 +0300

    while loop concepts

---
+++ while.py
@@ -0,0 +1,15 @@
+# while loop
+
+number = 0
+while number < 6:
+    print("Current number:", number)
+    number += 1
+
+
+# while else
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
+else:
+    print("Counting completed")
