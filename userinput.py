0727ece9ad32eb4d1b7be1a468c10de77ed9aa29
Lloyd <lloydbrownjnr@gmail.com>
2025-10-07 10:06:49 +0300

    day 2 - userinput.py

---
+++ userinput.py
@@ -0,0 +1,19 @@
+# input function - taking user input -- input() function
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+height = input("enter your height: ")
+is_student = input("are you a student (True/False): ")
+
+print("name is", name)
+print("age is", age)
+print("height is", height)
+print("is_student is", is_student)
+
+
+# area of rectangle
+length = input("enter length of rectangle: ")
+width = input("enter width of rectangle: ")
+
+area = float(length) * float(width)
+print("area of rectangle is", area)
